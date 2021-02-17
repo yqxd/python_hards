a55f8f47ea5a3e20080c88fa09543298328db8a5
yqxd <xuz19556@gmail.com>
2021-02-17 16:00:33 +0800

    60

---
+++ 0060PermutationSequence.py
@@ -0,0 +1,55 @@
+"""
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+
+
+Example 1:
+
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+Example 3:
+
+Input: n = 3, k = 1
+Output: "123"
+
+
+Constraints:
+
+1 <= n <= 9
+1 <= k <= n!
+"""
+
+
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        import math
+        k = k - 1
+        A = [i for i in range(1, n + 1)]
+        now = n - 1
+        result = ''
+        while now >= 0:
+            loc = k // math.factorial(now)
+            k = k % math.factorial(now)
+            result += str(A[loc])
+            A.pop(loc)
+            now -= 1
+        return result
