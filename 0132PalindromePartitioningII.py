841780b37069903a0a3b480cbc70f1f749495d4b
yqxd <xuz19556@gmail.com>
2021-03-04 09:48:48 +0800

    132

--- 0132PalindromePartitioningII.py
+++ 0132PalindromePartitioningII.py
@@ -27,3 +27,29 @@ s consists of lower-case English letters only.
 """
 
 
+class Solution(object):
+    def minCut(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) <= 1:
+            return 0
+        d = [0, 0]
+        for index1 in range(2, len(s) + 1):
+            if self.isP(s[:index1]):
+                MP = 0
+            else:
+                MP = index1 - 1
+                for index2 in range(1, index1):
+                    if self.isP(s[index2:index1]):
+                        MP = min(MP, 1 + d[index2])
+            d.append(MP)
+        return d[-1]
+
+    def isP(self, s):
+        return s[::-1] == s
+
+
+A = Solution()
+print(A.minCut("aab"))
